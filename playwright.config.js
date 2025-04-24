9714ce023b17c9df9e41c92b07e76397460cdc7c
sebastiangoodunited <sebastian@goodunited.io>
2025-04-24 16:24:45 -0500

    Se actualiza dev conainer y configuraciones de reporte

--- playwright.config.js
+++ playwright.config.js
@@ -5,5 +5,7 @@ export default defineConfig({
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
+    trace: 'on',
   },
+  reporter: [['html', { open: 'never' }]], // o 'on', si querés que se abra automáticamente
 });
